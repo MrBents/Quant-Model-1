0c92fd11cb0e7dacd31146dd10f75e370a98cacc
Gabriel Goldszmidt <gg675@nova.edu>
2015-11-22 08:32:14 -0500

    fixed bugs in arbitrage

--- Hack/src/gg675/Arbitrage.java
+++ Hack/src/gg675/Arbitrage.java
@@ -1,19 +1,22 @@
 package gg675;
 
-public class Arbitrage {
+import java.text.DecimalFormat;
 
+public class Arbitrage {
+	static DecimalFormat df = new DecimalFormat("#.###");
 	
 	public void arbi (double strike, double stock, double call, double put){
 		double left = stock + put;
-		double right = (strike + (strike * 0.0011)) + call;
+		double right = strike + call;
 		
-		if (left / right == 1){
+		if (left - right < 0){
 			System.out.println("No arbitrage");
+			System.out.println(df.format(left) + " " + "=" + " " + df.format(right));
 		}
 		
-		else if (left / right != 1){
+		else if (left - right > 0){
 			System.out.println("Arbitrage oportunity");
-			System.out.println(left - right);
+			System.out.println(df.format(left) + " " + "=" + " " + df.format(right));
 		}
 		
 	}
--- Hack/src/gg675/Main.java
+++ Hack/src/gg675/Main.java
@@ -3,7 +3,7 @@ package gg675;
 public class Main {
 
 	public static void main(String[] args) {
-		Skelleton sk = new Skelleton("TSLA");
+		Skelleton sk = new Skelleton("goog");
 
 	}
 
